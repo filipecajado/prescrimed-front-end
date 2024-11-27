8064a1d0a85f940cd03de9c328db58883572de36
Filipe Cajado <filipecajado@ucl.br>
2024-11-27 18:44:01 -0300

    update home page

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -1,11 +1,15 @@
 import { NgModule } from "@angular/core";
 import { HomeRoutingModule } from "./home-routing.module";
 import { HomeComponent } from "./main/home.component";
+import { MatCardModule } from "@angular/material/card";
+import { MatIconModule } from "@angular/material/icon";
 
 @NgModule({
     declarations: [HomeComponent],
     imports: [
-        HomeRoutingModule
+        HomeRoutingModule,
+        MatCardModule,
+        MatIconModule,
     ],
     providers: []
 })
